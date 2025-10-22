e19356cb7f8a2e570fb91d0e8900e8adbb8b8c68
rockyroed <roed-imp.vo97b@slmail.me>
2025-10-22 10:24:50 +0800

    add stats

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 	import Hero from './components/Hero/index.svelte';
 	import Stack from './components/Stack.svelte';
 	import About from './components/About.svelte';
+	import Stats from './components/Stats.svelte';
 </script>
 
 <main class="flex flex-col items-center justify-center px-24">
@@ -10,4 +11,5 @@
 	<Hero />
 	<Stack />
 	<About />
+	<Stats />
 </main>
---
+++ src/components/Stats.svelte
@@ -0,0 +1,21 @@
+<div class="flex w-full flex-col items-center gap-19 pt-14 pb-28">
+	<h2 class="font-jetbrains-mono text-h2 font-medium text-foreground">
+		{'<GithubStats />'}
+	</h2>
+	<div class="flex w-full gap-6">
+		<div class="flex w-full justify-center rounded-xl bg-background">
+			<img
+				class="w-150"
+				src="https://github-readme-stats.vercel.app/api?username=rockyroed&theme=gruvbox&show_icons=true&hide_border=true&count_private=true&rank_icon=github"
+				alt="rockyroed's GitHub Stats"
+			/>
+		</div>
+		<div class="flex w-full justify-center rounded-xl bg-background">
+			<img
+				class="w-150"
+				src="https://github-readme-stats.vercel.app/api/top-langs/?username=rockyroed&theme=gruvbox&show_icons=true&hide_border=true&layout=compact"
+				alt="rockyroed's Most Used Languages"
+			/>
+		</div>
+	</div>
+</div>
