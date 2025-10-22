8173746d727337e2eb9e619e1f30f90f50abe6b4
rockyroed <roed-imp.vo97b@slmail.me>
2025-10-22 10:02:20 +0800

    add navbar

---
+++ src/components/Button/Navbar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { value }: { value: string } = $props();
+</script>
+
+<button
+	class="cursor-pointer rounded-full px-4 py-1.5 text-h5 font-semibold text-text hover:bg-background"
+>
+	{value}
+</button>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import NavbarButton from './Button/Navbar.svelte';
+</script>
+
+<div
+	class="sticky top-0 z-50 flex h-20 w-screen items-center justify-between bg-background-dim px-24 py-5"
+>
+	<div class="flex h-max w-auto items-center justify-start gap-x-1 text-foreground">
+		<img
+			src="/images/logo-transparent.png"
+			alt="logo"
+			class="w-12"
+			loading="lazy"
+			decoding="async"
+		/>
+		<h4 class="font-md font-jetbrains-mono text-h4 font-semibold">rockyroed</h4>
+	</div>
+	<div class="flex w-auto items-center justify-end gap-x-4">
+		<NavbarButton value="About" />
+		<NavbarButton value="Stats" />
+		<NavbarButton value="Services" />
+		<NavbarButton value="Projects" />
+		<NavbarButton value="Certificates" />
+		<NavbarButton value="Contact" />
+	</div>
+</div>
