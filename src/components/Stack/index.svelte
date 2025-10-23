20ff6cce1a268822f472a18bd2df2b88b3dc4001
rockyroed <roed-imp.vo97b@slmail.me>
2025-10-23 13:41:13 +0800

    modify stack container height

--- src/components/Stack/index.svelte
+++ src/components/Stack/index.svelte
@@ -2,7 +2,7 @@
 	import { list } from './list';
 </script>
 
-<div class="flex h-40 w-screen items-center justify-between bg-background px-24">
+<div class="flex w-screen items-center justify-between bg-background px-24 py-8">
 	{#each list as item}
 		<img src={`https://skillicons.dev/icons?i=${item}`} alt={item} />
 	{/each}
