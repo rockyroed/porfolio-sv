f72e28b142323ab1bb3236fe9096f3f0195026b9
rockyroed <roed-imp.vo97b@slmail.me>
2025-12-19 07:13:05 +0800

    update GitHub stats image sources to new domain and adjust language filters

--- src/components/Stats.svelte
+++ src/components/Stats.svelte
@@ -6,14 +6,14 @@
 		<div class="flex w-full justify-center rounded-xl bg-background">
 			<img
 				class="w-150"
-				src="https://github-readme-stats.vercel.app/api?username=rockyroed&theme=gruvbox&show_icons=true&hide_border=true&count_private=true"
+				src="https://deor-github-stats.vercel.app/api?username=rockyroed&theme=gruvbox&show_icons=true&hide_border=true&count_private=true"
 				alt="rockyroed's GitHub Stats"
 			/>
 		</div>
 		<div class="flex w-full justify-center rounded-xl bg-background">
 			<img
 				class="w-150"
-				src="https://github-readme-stats.vercel.app/api/top-langs/?username=rockyroed&theme=gruvbox&show_icons=true&hide_border=true&layout=compact&hide=html,css,nix,shell,glsl,scss"
+				src="https://deor-github-stats.vercel.app/api/top-langs/?username=rockyroed&theme=gruvbox&show_icons=true&hide_border=true&layout=compact&hide=html,nix,shell,glsl,scss,qml"
 				alt="rockyroed's Most Used Languages"
 			/>
 		</div>
