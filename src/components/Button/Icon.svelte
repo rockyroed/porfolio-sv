55a4f1ed67807cec0eddab4d43c845e6db0f53be
rockyroed <roed-imp.vo97b@slmail.me>
2025-12-19 15:37:39 +0800

    remove cursor pointer to icon buttons

--- src/components/Button/Icon.svelte
+++ src/components/Button/Icon.svelte
@@ -4,12 +4,12 @@
 
 {#if icon.startsWith('https://')}
 	<button
-		class="h-12 w-12 cursor-pointer rounded-xl bg-background p-2 transition-all duration-300 ease-in hover:scale-110"
+		class="h-12 w-12 rounded-xl bg-background p-2 transition-all duration-300 ease-in hover:scale-110"
 	>
 		<img src={icon} alt={icon} />
 	</button>
 {:else}
-	<button class="h-12 w-12 cursor-pointer transition-all duration-300 ease-in hover:scale-110">
+	<button class="h-12 w-12 transition-all duration-300 ease-in hover:scale-110">
 		<img src={`https://skillicons.dev/icons?i=${icon}`} alt={icon} />
 	</button>
 {/if}
