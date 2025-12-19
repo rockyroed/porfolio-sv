4dd21d8c3222c2bd8e9e5fcbfa4b5198e6006eb1
rockyroed <roed-imp.vo97b@slmail.me>
2025-12-19 17:15:12 +0800

    add deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,4 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [tailwindcss(), svelte()] });
+export default defineConfig({ base: '/portfolio-sv/', plugins: [tailwindcss(), svelte()] });
