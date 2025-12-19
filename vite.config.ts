96322bbda338564916110bda306d400216edfc99
rockyroed <roed-imp.vo97b@slmail.me>
2025-12-19 16:47:45 +0800

    change vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,4 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [tailwindcss(), svelte()] });
+export default defineConfig({ plugins: [tailwindcss(), svelte()], base: '/portfolio-sv/' });
