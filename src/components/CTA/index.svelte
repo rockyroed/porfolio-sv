5d3815192153de9dc15d503b6171b2ddca5f5b1b
rockyroed <roed-imp.vo97b@slmail.me>
2025-12-19 13:55:44 +0800

    center text

--- src/components/CTA/index.svelte
+++ src/components/CTA/index.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex w-screen flex-col items-center justify-center bg-foreground gap-8 px-24 py-20">
 	<h2 class="text-h2 font-medium text-background">Let’s Build Something Awesome Together!</h2>
-	<p class="text-base text-background">
+	<p class="text-base text-center text-background">
 		I’m always open to chatting about new projects, ideas, or opportunities. Whether it’s a
 		full-time/part-time role or a freelance gig, let’s make it happen.
 	</p>
